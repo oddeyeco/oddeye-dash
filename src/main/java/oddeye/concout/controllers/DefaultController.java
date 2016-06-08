b97ed1b5e156efda3b1e8cd7b67c6bc476884874
Vahan <vahan_a@mail.ru>
2016-06-08 16:28:47 +0400

    Front Beta 2

--- src/main/java/oddeye/concout/controllers/DefaultController.java
+++ src/main/java/oddeye/concout/controllers/DefaultController.java
@@ -8,6 +8,7 @@ package oddeye.concout.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,9 +20,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DefaultController {
     
    @RequestMapping(value = "/", method = RequestMethod.GET)
-   public String index(ModelMap map) {
-       map.put("msg", "Hello Spring 4 Web MVC!");
+   public String index(ModelMap map) {           
+       map.put("body", "dashboard");
        return "index";
    }
     
+   @RequestMapping(value = "/{templatename}", method = RequestMethod.GET)
+   public String bytemplate(@PathVariable(value = "templatename") String templatename,ModelMap map) {       
+       map.put("body", templatename);
+       return "index";
+   }   
 }
