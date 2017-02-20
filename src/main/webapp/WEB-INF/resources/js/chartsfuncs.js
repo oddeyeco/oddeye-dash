c180ed966be56f70923dc84e2dd04a74c1b8e98b
vahan <vahan@vahan-pc>
2017-02-20 15:07:44 +0400

    26

--- src/main/webapp/WEB-INF/resources/js/chartsfuncs.js
+++ src/main/webapp/WEB-INF/resources/js/chartsfuncs.js
@@ -616,62 +616,64 @@ var format_metric = function (params, type = "m") {
             }
             case 3:
             case 4:
-            case 5:
+//            case 5:
             {
                 val = (val / Math.pow(divatior, 3));
                 metric = " k";
                 break;
             }
+            case 5:
             case 6:
-            case 7:
-            case 8:
+            case 7:            
             {
                 val = (val / Math.pow(divatior, 6));
                 metric = " M";
                 break;
             }
+            case 8:
             case 9:
-            case 10:
-            case 11:
+            case 10:            
             {
                 val = (val / Math.pow(divatior, 9));
                 metric = " G";
                 break;
             }
+            case 11:
             case 12:
             case 13:
-            case 14:
+            
             {
                 val = (val / Math.pow(divatior, 12));
                 metric = " T";
                 break;
             }
+            case 14:
             case 15:
-            case 16:
-            case 17:
+            case 16:            
             {
                 val = (val / Math.pow(divatior, 15));
                 metric = " P";
                 break;
             }
+            case 17:
             case 18:
-            case 19:
-            case 20:
+            case 19:            
             {
                 val = (val / Math.pow(divatior, 18));
                 metric = " E";
                 break;
             }
+            case 20:
             case 21:
-            case 22:
-            case 23:
+            case 22:            
             {
                 val = (val / Math.pow(divatior, 21));
                 metric = " Z";
                 break;
             }
+            case 23:
             case 24:
-            case 25:
+            case 25:            
             case 26:
             {
                 val = (val / Math.pow(divatior, 24));
