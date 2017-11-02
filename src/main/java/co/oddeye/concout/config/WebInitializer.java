8456c2e98d0e51db77e647ee0ce092ee3a7801dd
vahan <vahan@Vahan>
2017-11-02 10:12:29 +0400

    WebInitializer

---
+++ src/main/java/co/oddeye/concout/config/WebInitializer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.oddeye.concout.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * @author vahan
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Config.class, SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.addListener(org.springframework.web.util.Log4jConfigListener.class);
+        super.onStartup(servletContext);
+    }
+
+}
