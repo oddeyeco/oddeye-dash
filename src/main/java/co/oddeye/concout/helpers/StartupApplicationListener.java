b64252673ebc9d49b38e270ca9d8e9c1e2e8f09b
vahan <vahan@Vahan>
2017-12-14 12:13:01 +0400

    Fix Time Zones

---
+++ src/main/java/co/oddeye/concout/helpers/StartupApplicationListener.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.oddeye.concout.helpers;
+
+import co.oddeye.concout.dao.HbaseUserDao;
+import co.oddeye.concout.model.OddeyeUserModel;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author vahan
+ */
+@Component
+public class StartupApplicationListener implements
+        ApplicationListener<ContextRefreshedEvent> {
+ 
+    private final Logger LOGGER = LoggerFactory.getLogger(StompConnectedEvent.class);
+ 
+    public static int counter;
+    @Autowired
+    private HbaseUserDao Userdao;    
+ 
+    @Override public void onApplicationEvent(ContextRefreshedEvent event) {
+//        LOGGER.info("Increment counter");        
+        if (counter==0)
+        {
+            List<OddeyeUserModel> users = Userdao.getAllUsers(true);
+            users.forEach((user) -> {
+                Userdao.updateMetaList(user);
+            });
+        }
+        counter++;
+    }
+}
